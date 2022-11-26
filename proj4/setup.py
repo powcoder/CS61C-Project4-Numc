f969eee57c673b2d75869d2272e1d634ee19008a
powcoder <powcoder@163.com>
2022-11-26 20:30:54 +0800

    CS61C Project4 Numc

---
+++ proj4/setup.py
@@ -0,0 +1,28 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+from distutils.core import setup, Extension
+import sysconfig
+
+def main():
+    CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
+    LDFLAGS = ['-fopenmp']
+    setup(name="numc",
+          version="0.0.1",
+          description="numc matrix operations",
+          ext_modules=[
+            Extension("numc",
+                      sources=["src/numc.c", "src/matrix.c"],
+                      extra_compile_args=CFLAGS,
+                      extra_link_args=LDFLAGS,
+                      language='c')
+          ])
+
+
+if __name__ == "__main__":
+    main()
